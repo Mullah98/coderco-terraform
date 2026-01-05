2abde62f145aeaa65ecbb20f135635e41e3e3503
Mullah98 <ibz_374@hotmail.com>
2026-01-05 16:15:24 +0000

    add provider aws config + tag for aws import resource

--- terraform-aws-demo/ec2.tf
+++ terraform-aws-demo/ec2.tf
@@ -2,3 +2,11 @@ resource "aws_instance" "this" {
   ami                     = "ami-0a0ff88d0f3f85a14"
   instance_type           = "t3.micro"
 }
+
+resource "aws_instance" "import" {
+  ami                     = "ami-0a0ff88d0f3f85a14"
+  instance_type           = "t3.micro"
+  tags = {
+    Name = "terraform-import"
+  }
+}
