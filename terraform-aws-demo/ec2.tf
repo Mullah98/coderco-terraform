5296cbc7d34a349d29e93425055de023421cf27c
Mullah98 <ibz_374@hotmail.com>
2026-01-06 15:48:42 +0000

    Practice using variables in terraform

--- terraform-aws-demo/ec2.tf
+++ terraform-aws-demo/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0a0ff88d0f3f85a14"
-  instance_type           = "t3.micro"
+  instance_type           = var.instance_type
 }
 
 resource "aws_instance" "import" {
   ami                     = "ami-0a0ff88d0f3f85a14"
-  instance_type           = "t3.micro"
+  instance_type           = var.instance_type
   tags = {
     Name = "terraform-import"
   }
