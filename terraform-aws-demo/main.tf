23680891e69f0f93bd802aaa7bef7804acc70e28
Mullah98 <ibz_374@hotmail.com>
2026-01-09 00:19:40 +0000

    modifiy variables

--- terraform-aws-demo/main.tf
+++ terraform-aws-demo/main.tf
@@ -1,4 +1,5 @@
 module "ec2" {
   source = "./modules/ec2"
   instance_type = var.instance_type
+  instance_ami = var.instance_ami
 }
---
+++ terraform-aws-demo/terraform.tfvars
@@ -0,0 +1,2 @@
+instance_type = "t3.micro"
+instance_ami = "ami-0a0ff88d0f3f85a14"
